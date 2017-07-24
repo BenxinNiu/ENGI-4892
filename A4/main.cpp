9a18123a993e37435d23d0a2e80358e3c90ade90
Benxin Niu <bn2645@mun.ca>
2017-07-23 22:51:33 -0230

    added testing method and comments

--- A4/main.cpp
+++ A4/main.cpp
@@ -9,24 +9,24 @@ HeapPriorityQueue<int, std::string> queue;
 
 std::ifstream infor;
 infor.open("tickets.txt");
-if(infor.is_open()){
+if(infor.is_open()){  // file opened successfully
 int num;
 std::string ticket;
-while(!infor.eof()){
+while(!infor.eof()){  // not end of file
 infor>>num;
 infor>>ticket;
 std::cout<<std::endl<<"inserting a ticket"<<std::endl;
-queue.enqueue(num,ticket);
-queue.print();
+queue.enqueue(num,ticket);   // insert a ticket
+queue.print();              // print the content of queue after each insertion
 }
 std::cout<<std::endl<<"insert complete!"<<std::endl;
 }
 
-for(int i=0;i<7;i++){
+for(int i=0;i<7;i++){   // dequeue 7 times (last time should fail because there will be nothing in the queue)
 std::cout<<std::endl;
-std::cout<<"removed "<<queue.dequeue()<<std::endl;
+std::cout<<"removed "<<queue.dequeue()<<std::endl; // perform dequeue
 std::cout<<"what's left"<<std::endl;
-queue.print();
+queue.print(); // print heap after each
 std::cout<<std::endl;
 }
   return 0;
