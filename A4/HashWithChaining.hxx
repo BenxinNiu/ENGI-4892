b20e3673752706ac0c9321dfa8e0dd1135a20aa0
Benxin Niu <bn2645@mun.ca>
2017-07-23 22:48:45 -0230

    added comments

--- A4/HashWithChaining.hxx
+++ A4/HashWithChaining.hxx
@@ -16,7 +16,7 @@ class HashWithChaining : public HashTable<K,V>
 {
   public:
     // c'tor (initializes hashcode lambda function and size)
-    HashWithChaining(std::function<int(K)> hc) : hashcode(hc), size(0),items(0) {
+    HashWithChaining(std::function<int(K)> hc) : hashcode(hc), size(0),items(0),key_examined(0){
     }
 
     // Add key/value pair to the hashtable by using 'key' to determine
@@ -25,14 +25,16 @@ class HashWithChaining : public HashTable<K,V>
     // post-condition: 'value' is added at position determined by 'key'
     bool insert(const K& key, const V& value) override
     {
-      int index=hashcode(key)%10;
-      if(data[index].size()==0){
-      size++;
-      bucket_location.push_back(index);
+      int index=hashcode(key)%10;    // hash function to generate index
+      if(data[index].size()==0){    // if there is no existing bucket at this location
+      key_examined++;               // examined one key
+      size++;                       // increment the size(how many space taken in the array)
+      bucket_location.push_back(index); // record the location of new bucket for later access
       }
       if(index<capacity){
-      data[index].push_back(HTEntry(key,value));
-      items++;
+      key_examined++;              // examined one key
+      data[index].push_back(HTEntry(key,value)); // insert data
+      items++;                     // number of items in the table
       }
       else
       return false;
@@ -42,12 +44,15 @@ class HashWithChaining : public HashTable<K,V>
     // post-condition: the value associated with 'key', else nullptr
     const V* find(const K& key) override
     {
-      int index=hashcode(key)%10;
+      int index=hashcode(key)%10;  // hash function to generate index
       if(index<capacity)
-      for(int i=0; i<data[index].size();i++)
-        if((data[index][i]).key==key)
-        return &(data[index][i]).value;
-        return nullptr;
+      for(int i=0; i<data[index].size();i++){  // traverse the bucket at this location
+        key_examined++;                        // examined one key
+        if((data[index][i]).key==key)         //compare key
+        return &(data[index][i]).value;      // return address of value is keys are matched
+}
+        return nullptr;  // not found
+
 }
     // pre-condition:  a valid hashtable
     // post-condition: return the load factor; hashtable is not modified
@@ -55,9 +60,9 @@ class HashWithChaining : public HashTable<K,V>
     {
       cout<<"item: "<<items<<endl;
       cout<<"size: "<<size<<endl;
-      float a=size;
+      float a=items; // number of items in the table&& convet to float
       float b=capacity;
-      return a/b;
+      return a/b;   // calculate load factor
     }
 
     // pre-condition:  a valid hashtable
@@ -65,7 +70,7 @@ class HashWithChaining : public HashTable<K,V>
     //                 hashtable is not modified
     int totalKeysExamined() const override
     {
-      return items;
+      return key_examined; // how many times the program has accessed a key in table
     }
 
     // pre-condition:  a valid hashtable
@@ -73,14 +78,15 @@ class HashWithChaining : public HashTable<K,V>
     void print() const override
     {
       int location;
-     for(int i=0;i<bucket_location.size();i++)
-      print_bucket(bucket_location[i]);
+     for(int i=0;i<bucket_location.size();i++)  //print data stored in each non-empty bucket
+      print_bucket(bucket_location[i]);    // the function to print data in a bucket
 cout<<"total "<<items<<" students which are stored in "<<size<<" different location of hashtable"<<endl;
     }
+
     void print_bucket(int pos) const{
 cout<<"Data in bucket which is at "<<pos<<"th location of table"<<endl;
-      for(int i=0; i<data[pos].size();i++)
-        cout<<data[pos][i].key<<" "<<data[pos][i].value<<endl;
+      for(int i=0; i<data[pos].size();i++)  // traverse the bucket
+        cout<<data[pos][i].key<<" "<<data[pos][i].value<<endl;  // print information
         cout<<endl<<endl;
     }
 
@@ -94,9 +100,10 @@ cout<<"Data in bucket which is at "<<pos<<"th location of table"<<endl;
     };
     // A bucket array of vectors (each vector contains type 'HTEntry')
     vector<HTEntry> data[capacity];
-    int size;
-    int items;
-    vector <int> bucket_location;
+    int size;   // how many nonempty bucket in the table
+    int items;  // how many items are in the table
+    int key_examined; // how many times the program has accessed a key in table
+    vector <int> bucket_location;  // a vector to store the location of each non-empty bucket
 };
 
 
